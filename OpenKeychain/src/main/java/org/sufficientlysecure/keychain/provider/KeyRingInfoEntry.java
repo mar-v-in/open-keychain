8c7158010e5e2d7c349d41bc3c9b64c984f66780
mar-v-in <github@rvin.mooo.com>
2014-08-13 13:47:14 +0200

    make KeyRingInfoEntry parcelable

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/provider/KeyRingInfoEntry.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/provider/KeyRingInfoEntry.java
@@ -1,16 +1,32 @@
 package org.sufficientlysecure.keychain.provider;
 
 import android.content.ContentValues;
+import android.os.Parcel;
+import android.os.Parcelable;
 import org.sufficientlysecure.keychain.provider.KeychainContract;
 
-public class KeyRingInfoEntry {
+public class KeyRingInfoEntry implements Parcelable {
+
+    public static final Creator<KeyRingInfoEntry> CREATOR = new Creator<KeyRingInfoEntry>() {
+        @Override
+        public KeyRingInfoEntry createFromParcel(Parcel source) {
+            return new KeyRingInfoEntry(source.readByte() == 1, source.readInt(), source.readString(),
+                    source.readLong(), source.readLong());
+        }
+
+        @Override
+        public KeyRingInfoEntry[] newArray(int size) {
+            return new KeyRingInfoEntry[size];
+        }
+    };
+
     private final boolean visible;
     private final int reason;
     private final String source;
     private final long importDate;
     private final long updateDate;
 
-    public KeyRingInfoEntry(long updateDate, long importDate, String source, int reason, boolean visible) {
+    public KeyRingInfoEntry(boolean visible, int reason, String source, long importDate, long updateDate) {
         this.updateDate = updateDate;
         this.importDate = importDate;
         this.source = source;
@@ -26,5 +42,19 @@ public class KeyRingInfoEntry {
         values.put(KeychainContract.KeyRingInfo.IMPORT_DATE, importDate);
         values.put(KeychainContract.KeyRingInfo.UPDATE_DATE, updateDate);
         return values;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeByte((byte) (visible ? 1 : 0));
+        dest.writeInt(reason);
+        dest.writeString(source);
+        dest.writeLong(importDate);
+        dest.writeLong(updateDate);
     }
 }
